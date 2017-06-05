e72da9434463fb3f485395d46b6ccd4ca8491aee
Seana Curran <shaunacurran@shaunas-mbp.attlocal.net>
2017-06-04 21:49:24 -0500

    updated style

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+}
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	$(".mainArea").html(startScreen);
+});
