b488bc214299c764b06ec36c5513c2096252092d
Seana Curran <shaunacurran@shaunas-mbp.attlocal.net>
2017-06-04 22:58:25 -0500

    updated variables js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,24 @@ $(document).ready(function() {
 
 }
 
-function initialScreen() {
-	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
-	$(".mainArea").html(startScreen);
-});
+var questionArray = [
+		"Who did Zeus overthrow to claim his throne as King of the Gods?", 
+		"Who was known as the twice-born god?", 
+		"Who accompanied the God of War, Ares, into battle?", 
+		"Who was the mother of Hermes, the messenger god?", 
+		"Who were the twin Olympians?", 
+		"What side of the Trojan War was Apollo on?", 
+		"Who slayed the dragon, Ladon?", 
+		"How many years did it take for Odysseus to return home?"
+		];
+
+var answerArray = [
+		["Gaea", "Kronos", "Hyperion", "Thanatos"], 
+		["Dionysus","Dolos","Amphitrite","Macaria"], 
+ 		["Phobos & Deimos", "Enyo & Nike", "Nike & Eris", "Athena & Otrera"], 
+ 		["Semele","Hera","Leto","Maia"], 
+ 		["Hypnos & Thanatos", "Enyo & Ares", "Apollo & Artemis", "Phobos & Deimos"],
+  		["Troy","Greece","Neutral"],
+  		["Perseus", "Heracles", "Theseus", "Bion"],
+  	  	["15","25","10","2"]
+  	  	];
